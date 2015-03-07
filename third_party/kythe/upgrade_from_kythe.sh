5242f3c2790175aafdc1d7e66fabfcfd406f391b
Caitlin Sadowski <supertri@google.com>
2015-03-06 17:14:21 -0800

    Fixed script to use rsync, and actually update the README file.

--- third_party/kythe/upgrade_from_kythe.sh
+++ third_party/kythe/upgrade_from_kythe.sh
@@ -23,23 +23,7 @@ echo "Kythe repository: $KYTHE_REPO" >&2
 COMMIT=$(git --git-dir="$KYTHE_REPO/.git" rev-parse HEAD)
 echo "Upgrading third_party/kythe to version $COMMIT" >&2
 
-kythe_paths=(
- "proto"
- "go/platform/analysis"
- "go/platform/delimited"
- "go/platform/kindex"
- "java/com/google/devtools/kythe/common"
- "java/com/google/devtools/kythe/extractors/shared"
- "java/com/google/devtools/kythe/platform/shared"
- "java/com/google/devtools/kythe/platform/java"
- "java/com/google/devtools/kythe/platform/java/filemanager"
- )
-
-for path in "${kythe_paths[@]}"; do
-  rm -rf third_party/kythe/$path
-  mkdir -p third_party/kythe/$path
-  cp $KYTHE_REPO/kythe/$path/* third_party/kythe/$path
-done
+rsync --delete --files-from=third_party/kythe/imported_paths_from_kythe $KYTHE_REPO/kythe third_party/kythe
 
 # Fix all the imports
 grep -lR '"third_party/kythe/' third_party/kythe | xargs sed -i 's#"third_party/kythe/#"third_party/kythe/#g'
