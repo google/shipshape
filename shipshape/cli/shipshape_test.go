0ca7f581b534bd201b4ab37d2e90cfee58ebf085
R Samuel Klatchko <rsk@google.com>
2015-10-07 16:57:22 -0700

    First version of a go test to replace end_to_end_test.sh. This is still a work in progress.

--- shipshape/cli/shipshape_test.go
+++ shipshape/cli/shipshape_test.go
@@ -2,10 +2,83 @@ package cli
 
 import (
 	"testing"
+
+	rpcpb "shipshape/proto/shipshape_rpc_proto"  
 )
 
-// TODO(ciera): Make a testdata directory that contains test files
-// for all these tests.
+func countFailures(resp rpcpb.ShipshapeResponse) int {
+	failures := 0
+	for _, analyzeResp := range resp.AnalyzeResponse {
+		failures += len(analyzeResp.Failure)
+	}
+	return failures
+}
+
+func countNotes(resp rpcpb.ShipshapeResponse) int {
+	notes := 0
+	for _, analyzeResp := range resp.AnalyzeResponse {
+		notes += len(analyzeResp.Note)
+	}
+	return notes
+}
+
+func countCategoryNotes(resp rpcpb.ShipshapeResponse, category string) int {
+	notes := 0
+	for _, analyzeResp := range resp.AnalyzeResponse {
+		for _, note := range analyzeResp.Note {
+			if *note.Category == category {
+				notes += 1
+			}
+		}
+	}
+	return notes
+}
+
+func TestBasic(t *testing.T) {
+	options := Options{
+		File:                "shipshape/cli/testdata/workspace1",
+		ThirdPartyAnalyzers: []string{},
+		Build:               "maven",
+		TriggerCats:         []string{"PostMessage", "JSHint"},
+		Dind:                false,
+		Event:               "manual", // TODO: const
+		Repo:                "gcr.io/shipshape_releases", // TODO: const
+		StayUp:              true,
+		// TODO(rsk): current e2e test require tag to be specified on
+		// the command line. Furthermore, if the tag is "local" it builds
+		// containers and tags them.
+		Tag:                 "prod",
+		// TODO(rsk): current e2e test can be ru both with & without kythe.
+		LocalKythe:          false,
+	}
+	var allResponses rpcpb.ShipshapeResponse 
+	options.HandleResponse = func(shipshapeResp *rpcpb.ShipshapeResponse, _ string) error {
+		allResponses.AnalyzeResponse = append(allResponses.AnalyzeResponse, shipshapeResp.AnalyzeResponse...)
+		return nil
+	}
+
+	numNotes, err := New(options).Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if countFailures(allResponses) != 0 {
+		t.Errorf("Expected 0 failures: %v", allResponses)
+	}
+	if numNotes != countNotes(allResponses) {
+		t.Errorf("Disagreement between returned count (%v) and count of notes: %v", numNotes, allResponses)
+	}
+	if numNotes != 10 {
+		t.Errorf("Expected 10 notes, got %v: %v", numNotes, allResponses)
+	}
+	jshintNotes := countCategoryNotes(allResponses, "JSHint")
+	if jshintNotes != 8 {
+		t.Errorf("Expected 8 notes from JSHint, got %v: %v", jshintNotes, allResponses)
+	}
+	postMessageNotes := countCategoryNotes(allResponses, "PostMessage")
+	if postMessageNotes != 2 {
+		t.Errorf("Expected 2 notes from PostMessage, got %v: %v", postMessageNotes, allResponses)
+	}
+}
 
 func TestExternalAnalyzers(t *testing.T) {
 	// Replaces part of the e2e test
